c3c49fb0f717ecb7cff41994df14e322d9cdf4ac
Danila <golubev.d.a@mail.ru>
2020-03-09 16:06:38 +0300

    add properties

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
  function bs() {
    serveSass ();
@@ -17,6 +18,9 @@ const sass = require('gulp-sass');
  function serveSass() {
    return src("./sass/*.sass")
        .pipe(sass())
+       .pipe(autoprefixer({
+         cascade: false
+     }))
        .pipe(dest("./css"))
        .pipe(browserSync.stream());
 };
